ce27ed6b918c3dc9bd43cf50c153fc05e405b172
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-02-18 20:39:02 +0200

    adding copyright

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,16 +6,25 @@ import FooterSubscribe from "./FooterSubscribe";
 
 const Footer = () => {
     return (
-        <div className="bg-black text-white flex flex-col flex-wrap p-6">
+        <div className="bg-black text-white flex flex-col  gap-y-8">
             {/* First Row */}
             <FooterInfo />
 
-            {/* Second Row - 2 Columns on md+ screens */}
-            <div className="flex flex-col gap-6 md:grid md:grid-cols-2">
+            {/* Second Row - Responsive Grid */}
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
                 <FooterSocial />
                 <FooterServices />
                 <FooterGallery />
                 <FooterSubscribe />
+            </div>
+
+            {/* Copyright Section */}
+            <div style={{ backgroundColor: "rgba(34, 34, 34, 1)" }} className="p-4 text-center text-white">
+                <p>
+                    Copyright © 2024 
+                    <span className="text-[rgba(252,100,65,1)]"> edunity </span> 
+                    || All Rights Reserved
+                </p>
             </div>
         </div>
     );
