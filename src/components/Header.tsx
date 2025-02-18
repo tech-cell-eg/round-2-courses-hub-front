054673da31b2274ac82faa3a5b00639f5dde18ea
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-02-18 18:41:44 +0200

    uppercase header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<HeaderProps> = ({ pageName }) => {
       style={{ backgroundImage: `url(/Header.jfif)` }}
     >
       <div className="absolute inset-0 flex items-center justify-center flex-col gap-3">
-        <h1 className="text-black text-4xl font-bold">{pageName}</h1>
+        <h1 className="text-black text-4xl font-bold uppercase">{pageName}</h1>
         <p>Home // {pageName}</p>
       </div>
     </div>
