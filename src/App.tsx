6b727be2d0bc31753552dd69be938bda1e021a0a
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-02-18 18:42:13 +0200

    update route pagename

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ const App: React.FC = () => {
             <Navbar />
             <Routes>
                 {routes.map((route) => {
-                    const pageName =
-                        route.path === "/" ? "Home" : route.path.split("/")[1]; // Example for extracting page name
+                  
                     return (
                         <Route
                             key={route.path}
@@ -20,7 +19,7 @@ const App: React.FC = () => {
                             element={
                                 route.path !== "/" ? (
                                     <>
-                                        <Header pageName={pageName} />
+                                        <Header pageName={route.pageName} />
                                         <section className="px-5 md:px-20 lg:px-30">
                                             <route.component />
                                         </section>
