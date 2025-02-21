2ad1699315bbb61ae11a1cd76e3b154d546a40e2
Ahmed Moawad <ahmedmoawad550@gmail.com>
2025-02-21 16:11:53 +0200

    create instructor profile

---
+++ src/pages/Instructors/data.ts
@@ -0,0 +1,12 @@
+import { InstructorProps } from "../../types/types.ts";
+
+export const instructorCards: InstructorProps[] = [
+  { id: "1", img: "/instructor1.png", name: "Micheal Hammond", job: "Teacher" },
+  { id: "2", img: "/instructor2.png", name: "Sarah Johnson", job: "Teacher" },
+  { id: "3", img: "/instructor3.png", name: "James Anderson", job: "Teacher" },
+  { id: "4", img: "/instructor4.png", name: "Emily Roberts", job: "Teacher" },
+  { id: "5", img: "/instructor5.png", name: "David Smith", job: "Teacher" },
+  { id: "6", img: "/instructor6.png", name: "Jessica Brown", job: "Teacher" },
+  { id: "7", img: "/instructor7.png", name: "William Scott", job: "Teacher" },
+  { id: "8", img: "/instructor8.png", name: "Olivia Davis", job: "Teacher" },
+];
